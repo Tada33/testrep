7142792e24813f3e01179695563eb7644576d484
Tada33 <tadanari33@gmail.com>
2021-08-30 01:48:18 +1000

    80 % completed

---
+++ Assets/Scenes/PrintAndHide.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+  
+
+    public int i = 4;
+
+    public Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (i > 1)
+        {
+            Debug.Log(gameObject.name + " : " + i);
+            i++;
+        }
+
+    }
+}
